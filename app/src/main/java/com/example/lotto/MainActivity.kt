23f3375f39f88e404949603eb5fa966f39996418
jeongjeonghwan <tonyjjh0513@naver.com>
2022-11-16 17:48:35 +0900

    회차번호 입력 후 날짜까지 추가(테스트 필요)

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.lotto
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
+import com.example.lotto.data.remote.api.Status
 import com.example.lotto.ui.lotto.LottoViewModel
 
 class MainActivity : ComponentActivity() {
@@ -24,35 +29,68 @@ class MainActivity : ComponentActivity() {
     }
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnrememberedMutableState")
     @Preview
     @Composable
     fun LottoMain() {
-
         MaterialTheme {
-            val typography = MaterialTheme.typography
-            Column{
-                SimpleOutlinedTextField()
-                Button(onClick = { /*TODO*/ }) {
-                    
+            Column {
+                var lottoNumber by remember { mutableStateOf("") }
+                var date by remember { mutableStateOf("") }
+
+
+                OutlinedTextField(
+                    value = lottoNumber,
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    onValueChange = { lottoNumber = it },
+                    label = { Text("회차") }
+                )
+                Button(
+                    onClick = {
+                        if (lottoNumber.isEmpty()) {
+                            Toast.makeText(this@MainActivity, "값을 입력해주세요", Toast.LENGTH_LONG).show()
+                        } else {
+                            lottoViewModel.getRecentWinningNumber(lottoNumber.toInt())
+
+                            lottoViewModel.lottoApiResponseLiveData.observe(this@MainActivity) { response ->
+                                when (response.status) {
+                                    Status.LOADING -> {
+
+                                    }
+                                    Status.SUCCESS -> {
+                                        response.data?.let { lotto ->
+                                            date = lotto.drwNoDate
+//                                            binding.txtNumber.text =
+//                                                "${lotto.drwtNo1} ${lotto.drwtNo2}  " +
+//                                                        "${lotto.drwtNo3} ${lotto.drwtNo4} ${lotto.drwtNo5} ${lotto.drwtNo6} "
+                                        }
+                                    }
+                                    Status.ERROR -> {
+
+                                    }
+                                }
+
+                            }
+                        }
+
+                    }
+                ) {
+                    Text(text = "회차 조회")
                 }
+                Text(text = date)
             }
 
         }
 
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Preview
     @Composable
-    fun SimpleOutlinedTextField() {
-        var text by remember { mutableStateOf("회차 번호") }
-
-        OutlinedTextField(
-            value = text,
-            onValueChange = { text = it },
-            label = { Text("회차") }
-        )
+    fun StringResourceText(drwNoDate: String) {
+        Text(text = drwNoDate)
     }
+
 }
 
 
